efdc7c79933cad842cee41bc6375f3bb770b923b
segun ola <segunlalive@gmail.com>
2017-06-21 12:33:44 +0100

    added naira symbol to index page

--- index.js
+++ index.js
@@ -94,9 +94,7 @@ app.use(flash());
 
 // set some global variables
 app.use(function (req, res, next) {
-  // res.locals.success_msg = req.flash('success_msg');
-  // res.locals.error_msg = req.flash('error_msg');
-  // res.locals.error = req.flash('error');
+  res.locals.messages = messages(req, res);
   res.locals.user = req.user || null;
   next();
 });
