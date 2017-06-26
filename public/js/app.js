0a20a48aea567f6db4b96e2e12f40f47cd888496
segun ola <segunlalive@gmail.com>
2017-06-26 13:55:04 +0100

    cleaning out some artifacts

--- public/js/app.js
+++ public/js/app.js
@@ -4,19 +4,21 @@ $(document).ready(function () {
   $(".delete-link").on("click", function (e) {
     $target = $(e.target);
     const id = $target.attr("data-id");
-    if (confirm("Are you sure you want to delete this institution from " +
-                "list of institutions?")) {
-                  $.ajax({
-                    type: "DELETE",
-                    url: "/admin/institution/" + id.toString(),
-                    success: function (response) {
-                      alert(response);
-                      location.reload();
-                    },
-                    error: function (err) {
-                      alert('An error occured. Unable to delete institution');
-                    }
-                  })
-                }
+    if (confirm(
+        "Are you sure you want to delete this institution from " +
+        "list of institutions?")
+        ) {
+        $.ajax({
+          type: "DELETE",
+          url: "/admin/institution/" + id.toString(),
+          success: function (response) {
+            alert(response);
+            location.reload();
+          },
+          error: function (err) {
+            alert('An error occured. Unable to delete institution');
+          }
+        })
+      }
     });
 });
